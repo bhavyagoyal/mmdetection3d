bc29b547af7d0763480b9f45caf02bef54c010ba
Bhavya Goyal <bhavyagoyal777@gmail.com>
2024-02-07 10:42:41 -0600

    minor changes, added simulation for points min sbr

--- tools/dataset_converters/create_pkl.py
+++ tools/dataset_converters/create_pkl.py
@@ -3,6 +3,21 @@ import copy
 import numpy as np
 splits = ['train', 'val']
 SBR='1_50'
+
+
+## create clean point cloud with extra feature containing confidence/probability as one
+#BASE = '../../data/sunrgbd/points_clean/'
+#OUTBASE = '../../data/sunrgbd/points_clean7/'
+#for i in range(10335):
+#    fname = str(i+1).zfill(6) + '.bin'
+#    print(fname)
+#    points = np.fromfile(BASE + fname, dtype=np.float32)
+#    points = points.reshape(-1,6)
+#    ones = np.ones((points.shape[0], 1), dtype=np.float32)
+#    points = np.concatenate([points[:,:3], ones*2., points[:,3:]], 1)
+#    points.tofile(OUTBASE+fname)
+    
+
 for sp in splits:
     with open('../../data/sunrgbd/sunrgbd_infos_' + sp + '.pkl', 'rb') as f:
         data = pickle.load(f)
@@ -26,5 +41,5 @@ for sp in splits:
     #with open('../../data/sunrgbd/sunrgbd_infos_' + sp + '_' + SBRstr + '_shortvis.pkl', 'wb') as f:
     with open('../../data/sunrgbd/sunrgbd_infos_' + sp + '_shortvis.pkl', 'wb') as f:
         pickle.dump(data, f)
-    
+   
 
