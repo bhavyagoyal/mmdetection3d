5ca21337ac70eaa29d8fa512ac14e831aa165455
Bhavya Goyal <bhavyagoyal777@gmail.com>
2024-02-29 10:35:12 -0600

    updated flimbflie and visual.py

--- tools/dataset_converters/crop.py
+++ tools/dataset_converters/crop.py
@@ -3,5 +3,7 @@ import cv2
 fname = sys.argv[1]
 fnameout = sys.argv[2]
 img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
-img = img[275:975,300:1200,:]
+#img = img[275:975,300:1200,:] fps samples
+img = img[230:930,660:1130,:]
+#img = img[420:650,750:1150,:]
 cv2.imwrite(fnameout, img)
--- tools/dataset_converters/visual.py
+++ tools/dataset_converters/visual.py
@@ -3,41 +3,33 @@ import numpy as np
 from mmdet3d.visualization import Det3DLocalVisualizer
 import matplotlib
 import seaborn as sns
+import sys
 
 
-#points = np.fromfile('demo/data/sunrgbd/000017.bin', dtype=np.float32)
-#points = np.fromfile('../../data/sunrgbd/points_5_5_gtdepth/005050.bin', dtype=np.float32)
-#points = np.fromfile('pointscheckclean/points0.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/test_peaks-confidence-1-50-fps5000-sp03-1-0.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/denoise-softmax_1_50_005050.bin', dtype=np.float32)
-points = np.fromfile('data/sunrgbd/points_min2/0.3/argmax-filtering-sbr/1_50/005050.bin', dtype=np.float32)
-points = np.fromfile('data/sunrgbd/points_min2/0.048/gaussfilter-argmax-filtering-sbr/1_50/005050.bin', dtype=np.float32)
-#points = np.fromfile('data/sunrgbd/points_clean8/001743.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/5_50_argmax_filtering_conf_005050.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/5_5_gtdepth_005050.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/5_50_wsp_cf_005050.bin', dtype=np.float32)
-#points = np.fromfile('pointscheck/velodyne_000001.bin', dtype=np.float32)
-#points = np.fromfile('../../data/sunrgbd/points_bk/005050.bin', dtype=np.float32)
-#points = np.fromfile('/nobackup/bhavya/votenet/sunrgbd/matlab/testing_depth.bin', dtype=np.float32)
+fname = sys.argv[1]
+points = np.fromfile(fname, dtype=np.float32)
+#points = np.fromfile('data/sunrgbd/points_min2/1.0/score-denoised_sunrgbd1000/argmax-filtering-sbr/5_50_testing/pcl/000001.bin', dtype=np.float32)
+#points = np.fromfile('data/sunrgbd/points_gaussian/score-denoised/0.2/pcl/000001.bin', dtype=np.float32)
+#points = np.fromfile('data/sunrgbd/points_gaussian/0.2/000001.bin', dtype=np.float32)
+#points = np.fromfile('data/sunrgbd/points_min2/1.0/argmax-filtering-sbr/5_50/000001.bin', dtype=np.float32)
 print(points.shape)
 points = points.reshape(-1,8)
+
 #choices = np.random.choice(points.shape[0], 5000, replace=False)
 #points = points[choices]
 #points = points.reshape(-1,6)
 #points = points.reshape(-1, 6)[:,:3]
+
 #points_color = points[:,4]
-points_color = points[:,5:8]
+#points_color = points[:,5:8]
 #points_color = points_color/points_color.max()
 #points_color = sns.color_palette('coolwarm', as_cmap=True)(points_color)[:,:3]
+
 points = points[:,:3]
-#points = points.reshape(-1, 3)
 visualizer = Det3DLocalVisualizer()
-#mask = np.random.rand(points.shape[0], 3)
-#points_with_mask = np.concatenate((points, mask), axis=-1)
-# Draw 3D points with mask
-#visualizer.set_points(points, pcd_mode=2, vis_mode='add')
-visualizer.set_points(points, pcd_mode=2, vis_mode='add', points_color = points_color)
+
+visualizer.set_points(points, pcd_mode=2, vis_mode='add')
+#visualizer.set_points(points, pcd_mode=2, vis_mode='add', points_color = points_color)
     
-#visualizer.draw_seg_mask(points_with_mask)
 visualizer.show()
 
