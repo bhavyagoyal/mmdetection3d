ff3687feac4706e1ab0a9a96f40e7977270cdcee
Bhavya Goyal <bhavyagoyal777@gmail.com>
2024-02-29 10:36:23 -0600

    updated sorting etc

--- tools/dataset_converters/plot.py
+++ tools/dataset_converters/plot.py
@@ -17,16 +17,18 @@ matplotlib.rcParams['ytick.color'] = 'black'
 matplotlib.rcParams.update({'font.size': 12})
 
 #fig, ax = plt.subplots()
-x = ['0.1', '0.05', '0.02', '0.01']
-p1 = [10, 10, 10, 10]
-p2 = [20, 20., 20., 20]
-p3 = [54.29, 52.46, 38.49, 29.42]
-p4 = [52, 52, 52, 52]
+x = ['Clean', '0.1', '0.05', '0.02', '0.01']
+p3 = [58.61, 54.29, 52.46, 38.49, 29.42]
+p4 = [58.11, 52.89, 48.47, 35.13, 23.68]
+p1 = [58.61, 54.11, 50.03, 36.45, 25.33]
+p2 = [58.45, 49.23, 45.63, 26.44, 15.44]
+p5 = [58.34, 42.43, 38.77, 16.95, 11.34]
 
-#plt.plot(x, p1, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o NPD Filtering')
-#plt.plot(x, p2, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o FPS Cap')
-plt.plot(x, p4, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o Probability')
 plt.plot(x, p3, marker='D', linewidth=3.0, markersize=7.0, label='PPC')
+#plt.plot(x, p1, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o FPPS')
+#plt.plot(x, p2, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o NPD Filtering')
+#plt.plot(x, p5, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o FPPS & NPD Filtering')
+plt.plot(x, p4, marker='D', linewidth=3.0, markersize=7.0, label='PPC w/o Probability')
 plt.xlabel('SBR')
 plt.ylabel('mAP 3D Object Detection')
 plt.legend()
