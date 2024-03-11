782f24a8df836ac4914592278f3771203e51f929
Bhavya Goyal <bhavyagoyal777@gmail.com>
2024-03-11 16:04:33 -0500

    visualization changes

--- tools/dataset_converters/crop.py
+++ tools/dataset_converters/crop.py
@@ -4,15 +4,20 @@ fname = sys.argv[1]
 fnameout = sys.argv[2]
 img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
 #img = img[275:975,300:1200,:] #fps samples
+img = img[275:975,300:1100,:] #fps new example
 #img = img[230:930,660:1130,:]
 
-# exp 14
-#img = img[350:700,750:1100,:]
-#img = img[300:700,700:1150,:]
+# day 3 exp 5
+#img = img[400:750,700:1200,:] # front
 
 
-# exp 14
-img = img[400:650,750:1100,:] # top # front
+# day 4 exp 14
+#img = img[450:680,780:1100,:] # front
+#img = img[300:700,700:1150,:] # top
+
+
+# day 4 exp 19
+#img = img[400:650,750:1100,:] # top # front
 
 
 cv2.imwrite(fnameout, img)
