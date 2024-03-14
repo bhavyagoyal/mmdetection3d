df12081ec4866b7bc010e03d47a91c5b5eef7173
Bhavya Goyal <bhavyagoyal777@gmail.com>
2024-03-14 01:00:45 -0500

    visualization changes for supplementary first draft

--- tools/ppc_scripts/crop.py
+++ tools/ppc_scripts/crop.py
@@ -4,7 +4,7 @@ fname = sys.argv[1]
 fnameout = sys.argv[2]
 img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
 #img = img[275:975,300:1200,:] #fps samples
-img = img[275:975,300:1100,:] #fps new example
+#img = img[275:975,300:1100,:] #fps new example
 #img = img[230:930,660:1130,:]
 
 # day 3 exp 5
@@ -13,11 +13,23 @@ img = img[275:975,300:1100,:] #fps new example
 
 # day 4 exp 14
 #img = img[450:680,780:1100,:] # front
-#img = img[300:700,700:1150,:] # top
+#img = img[375:650,700:1150,:] # top
 
 
 # day 4 exp 19
-#img = img[400:650,750:1100,:] # top # front
+#img = img[400:650,750:1100,:] # front
+#img = img[375:650,800:1050,:] # top
 
+# day 5 exp 18
+#img = img[400:700,800:1110,:]  # front
+#img = img[400:680,825:1060,:] # top
+
+
+# day 5 exp 6
+#img = img[400:700,750:1110,:] # top # front
+
+
+# day 5 exp 11
+#img = img[400:700,800:1110,:] # top # front
 
 cv2.imwrite(fnameout, img)
